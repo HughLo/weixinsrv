64ac19e98ceea1804b7d57d5289c3c74d824a5ac
hughlo <lorfawit@gmail.com>
2014-08-28 22:20:42 +0800

    set GOPATH in build.go

--- build.go
+++ build.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"io"
 	"os/exec"
+	"path/filepath"
 )
 
 func CopyFile(srcName, dstName string) error {
@@ -28,15 +29,36 @@ func CopyFile(srcName, dstName string) error {
 }
 
 func main() {
-	//copy help.txt to be under bin folder so that the program can access the help file
-	err := CopyFile("./misc/help.txt", "./bin/help.txt")
+
+	//1. set GOPATH env variable
+	//take the directory of build.go as the GOPATH value
+	absPath, err := filepath.Abs(".")
 	if err != nil {
 		log.Println(err)
+		return
 	}
 
+	log.Printf("set GOPATH as: %s\n", absPath)
+
+	err = os.Setenv("GOPATH", absPath)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	//2. copy help.txt to be under bin folder so that the program can access the help file
+	err = CopyFile(filepath.Join(absPath, "/misc/help.txt"), 
+		filepath.Join(absPath, "/bin/help.txt"))
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	//3. install main package
 	cmd := exec.Command("go", "install", "-v", "main")
 	err = cmd.Run()
 	if err != nil {
 		log.Println(err)
+		return
 	}
 }
