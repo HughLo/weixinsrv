9c1f003d19e722dec5517b10e58b9c0cbf615156
hughlo <lorfawit@gmail.com>
2014-08-29 00:39:03 +0800

    add program running tool

---
+++ start_server.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"os/exec"
+)
+
+func main() {
+	//1. set GOPATH env variable
+	//take the directory of build.go as the GOPATH value
+	absPath, err := filepath.Abs(".")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	log.Printf("set GOPATH as: %s\n", absPath)
+
+	err = os.Setenv("GOPATH", absPath)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	//2. run server program
+	cmd := os.Command("sudo", "-E", "./bin/main")
+	err = cmd.Run()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+}
